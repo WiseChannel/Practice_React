8d6f934841c77bedb7233124a50a5e17bfe869cd
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-02-06 17:53:26 +0200

    Updates

---
+++ precache-manifest.bfa2b5d0f8c7dc506568dae417e75ffc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfea12d1e8675b171be7c69f18b6a60",
+    "url": "/Practice_React/index.html"
+  },
+  {
+    "revision": "3eba616f7aab799a42f3",
+    "url": "/Practice_React/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "1d3480d5d352a29b65ea",
+    "url": "/Practice_React/static/css/main.a2421d97.chunk.css"
+  },
+  {
+    "revision": "3eba616f7aab799a42f3",
+    "url": "/Practice_React/static/js/2.1a583a33.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Practice_React/static/js/2.1a583a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3480d5d352a29b65ea",
+    "url": "/Practice_React/static/js/main.b650bdc5.chunk.js"
+  },
+  {
+    "revision": "5daf11e3b64a093ee74b",
+    "url": "/Practice_React/static/js/runtime-main.d9df2486.js"
+  }
+]);
