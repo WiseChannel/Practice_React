75d3148eeed808bcbe003db1c2ed3647ae5bd2b3
Ilya <ilyaphotoshop17@gmail.com>
2020-02-29 17:08:09 +0200

    started learn new book React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoApp from './Components/test'
 
 function App() {
   return (
     <div className="App">
         <h1>Wise</h1>
+        <TodoApp/>
     </div>
   );
 }
